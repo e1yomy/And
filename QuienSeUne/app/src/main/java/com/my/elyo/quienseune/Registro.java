955b8dec684abbb998da6e8224a594d6c3013c62
HP\elyo_ <elyo_my@hotmail.com>
2016-11-08 08:45:37 -0700

    no message

--- QuienSeUne/app/src/main/java/com/my/elyo/quienseune/Registro.java
+++ QuienSeUne/app/src/main/java/com/my/elyo/quienseune/Registro.java
@@ -17,7 +17,7 @@ public class Registro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
-
+        //(Spinner)findViewById(R.id.spinner);
 
     }
     /*
