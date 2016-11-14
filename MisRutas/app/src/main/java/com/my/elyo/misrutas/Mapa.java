f125d6eea1cbec7d00a58deedcd28aa70c11982c
HP\elyo_ <elyo_my@hotmail.com>
2016-11-13 17:58:44 -0700

    Sólo falta dibujar las lineas

--- MisRutas/app/src/main/java/com/my/elyo/misrutas/Mapa.java
+++ MisRutas/app/src/main/java/com/my/elyo/misrutas/Mapa.java
@@ -12,6 +12,7 @@ import android.support.v4.content.ContextCompat;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -20,19 +21,16 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
 
-public class Mapa extends FragmentActivity implements OnMapReadyCallback {
+public class Mapa extends FragmentActivity implements OnMapReadyCallback, Runnable{
 
-    LatLng ubi; //ubicacion
-    Criteria criterio;
+
     private GoogleMap m;
-    private LocationManager manejador;
-    Location localizacion;
-    private String proveedor;
     Context contextR = this;
     public View vi;
     ArrayList<LatLng> lista=new ArrayList<>();
     B b;
-
+    int can;
+    Thread h1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +40,7 @@ public class Mapa extends FragmentActivity implements OnMapReadyCallback {
         mapFragment.getMapAsync(this);
         b= new B(contextR,"gps");
         hacerlista();
+        h1=new Thread(this);
     }
 
 
@@ -56,13 +55,14 @@ public class Mapa extends FragmentActivity implements OnMapReadyCallback {
         }
         if(lista.size()>0)
             mostrarlista();
-
-
+        h1.start();
     }
     public void hacerlista(){
         Cursor cr = b.selectAll(b.getReadableDatabase());
+
         try {
             if (cr.moveToFirst()) {
+                lista.clear();
                 do {
                     lista.add(new LatLng(cr.getDouble(1),cr.getDouble(2)));
                 } while (cr.moveToNext());
@@ -72,10 +72,48 @@ public class Mapa extends FragmentActivity implements OnMapReadyCallback {
         }
     }
     public void mostrarlista(){
-        for(int i=0;i< lista.size();i++)
-        {
-            m.addMarker(new MarkerOptions().position(lista.get(i)));
+        //hacer variable para el primer elemento
+        //hacer variable para el siguiente elemento
+        //si la lista es diferente de 0
+        m.clear();
+        can=lista.size();
+        if(lista.size()>0) {
+            for (int i = 0; i < lista.size() - 1; i++) {
+                //if(i==0)
+                //primerpunto = lista.get(i);
+                //segundo = lista.get(i+1);
+                //else
+                //
+                //
+                //
+                //
+                m.addMarker(new MarkerOptions().position(lista.get(i)));
+                m.animateCamera(CameraUpdateFactory.newLatLngZoom(lista.get(i),18));
+
+            }
         }
     }
 
+    @Override
+    public void run() {
+        while(true){
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        hacerlista();
+                        if(can<lista.size()) {
+                            mostrarlista();
+                        }
+                    }
+                    catch (Exception ex){}
+                }
+            });
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
